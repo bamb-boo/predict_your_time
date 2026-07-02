d79ec39c7eb8613c20fcb3899e14051e0b503fc0
bamb-boo <gsbarathraam18@gmail.com>
2026-07-02 19:49:51 +0530

    Update color.py

--- color.py
+++ color.py
@@ -2,15 +2,12 @@ import tkinter as tk
 import random 
 import time
 
-randtime=round(random.uniform(1.0, 4.0),2)
-userstart=0.0
-userduration=0.0
-pressed=True
-done=False
-total=int(((2.7+randtime)*1000))
-
 def color_on():
     label.config(text="", bg="#ADEBB3")
+
+def color_change():
+    label.config(bg="#8EC093", text="")
+    
 def color_off():
     global pressed, done
     pressed=False
@@ -27,7 +24,7 @@ def spacedown(event):
         label.config(bg="#ADEBB3", text="")
 
 def spaceup(event):
-    global pressed, done
+    global pressed, done, randtime
     if not done:
         return
     
@@ -35,15 +32,16 @@ def spaceup(event):
         pressed=False
         userduration=round(time.perf_counter()-userstart, 2)
 
-        accuracy=round((userduration/randtime)*100, 2)
+        accuracy=round((userduration/(randtime+1))*100, 2)
         if accuracy > 100:
             correct_acc = accuracy-100
             accuracy=100-correct_acc
 
         results=(
-            f"target {round(randtime, 2)}s \n"
+            f"target {round(randtime+1, 2)}s \n"
             f"your time {round(userduration, 2)}s \n"
-            f"accuracy {accuracy}%"
+            f"accuracy {round(accuracy, 2)}% \n"
+            f"click r to restart"
             )
         label.config(bg="#121212", fg="#10B981", text=results)
 
@@ -56,20 +54,34 @@ def two():
 def one():
     label.config(text="1")
 
+def begin():
+    global randtime, total, pressed, done, userstart
+    randtime=round(random.uniform(1.0, 4.0),2)
+    userstart=0.0
+    userduration=0.0
+    pressed=True
+    done=False
+    total=int(((4.4+randtime)*1000))
+
+    label.config(text="determine the length of the flash", bg="#FFFFFF", fg="#000000")
+    root.after(1000, three)
+    root.after(2000, two)
+    root.after(2800, one)
+    root.after(3600, color_on)
+    root.after(total, color_off)
+
+def restart(event):
+    global done
+    if done:
+        begin()
+
 root=tk.Tk()
 root.geometry("400x400")
-label=tk.Label(
-    root, text=("determine the length of the flash"),
-    bg="#FFFFFF", fg="#000000"
-)
+label=tk.Label(root)
 label.pack(expand=True, fill="both")
-root.after(1000, three)
-root.after(2000, two)
-root.after(2100, one)
-root.after(2700, color_on)
-root.after(total, color_off)
-
 root.bind("<KeyPress-space>", spacedown)
 root.bind("<KeyRelease-space>", spaceup)
+root.bind("<KeyPress-r>", restart)
 
+begin()
 root.mainloop()
